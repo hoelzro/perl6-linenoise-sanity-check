ec9967c64c36b1dd1fcf52b0742c0a8bf7c4f636
Rob Hoelz <rob@hoelz.ro>
2015-12-22 21:54:07 -0600

    Restore full Perl 6 build
    Now that I pray experimentation is done

--- verify-linenoise-ok.sh
+++ verify-linenoise-ok.sh
@@ -6,22 +6,19 @@ tmux -V
 
 export PATH=$PATH:/tmp/nom/bin:/tmp/nom/share/perl6/site/bin/
 
-#git clone https://github.com/rakudo/rakudo
-#git clone https://github.com/tadzik/panda
+git clone https://github.com/rakudo/rakudo
+git clone https://github.com/tadzik/panda
 
-#cd rakudo
-#perl Configure.pl --prefix=/tmp/nom --gen-moar && make install
+cd rakudo
+perl Configure.pl --prefix=/tmp/nom --gen-moar && make install
 
-#cd ../panda
-#perl6 bootstrap.pl
+cd ../panda
+perl6 bootstrap.pl
 
-#panda install Linenoise
+panda install Linenoise
 
-#tmux -L non-default new-session -d -s perl6 perl6
-MANPAGER=cat man tmux
-tmux -L non-default new-session -d -s cat cat
+tmux -L non-default new-session -d -s perl6 perl6
 tmux -L non-default send-keys s tab
 tmux -L non-default capture-pane
-tmux -L non-default list-buffers
 tmux -L non-default save-buffer -b 0 /tmp/out
 grep -q samecase /tmp/out || (echo "tab completion seems to be broken" && false)
